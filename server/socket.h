9ba1356417820ae7578ec2c1dce57510b0ccc99e
Quentin SANTOS <quentin.santos@worldonline.fr>
2012-11-18 17:14:06 +0100

    Separated server and client

---
+++ server/socket.h
@@ -0,0 +1,29 @@
+/*\
+ *  This is an awesome programm simulating awesome battles of awesome robot tanks
+ *  Copyright (C) 2012  Quentin SANTOS
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+\*/
+
+#ifndef SOCKET_H
+#define SOCKET_H
+
+int TCP_Connect (const char*, const char*);
+int TCP_ListenTo(const char*, const char*);
+int TCP_Listen  (const char*);
+int TCP_Accept  (int);
+
+void TCP_Timeout(int, int);
+
+#endif
