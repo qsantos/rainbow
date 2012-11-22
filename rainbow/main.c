065bd21a3b380a86a7014a4ddc2839218bb83966
Quentin SANTOS <quentin.santos@worldonline.fr>
2012-11-22 20:00:17 +0100

    raibow: improved command line

--- rainbow/main.c
+++ rainbow/main.c
@@ -7,74 +7,85 @@
 #include "rainbow.h"
 #include "md5.h"
 
-char stop = 0;
-void signal_handler(int sig)
+void usage(int argc, char** argv)
 {
-	(void) sig;
-	stop = 1;
+	(void) argc;
+
+	printf
+	(
+		"Usage: %s file slen l_chains n_chains mode\n"
+		"\n"
+		"mode:"
+		"  rtgen\n"
+		"  tests\n"
+		,
+		argv[0]
+	);
 }
 
 int main(int argc, char** argv)
 {
-	assert(argc >= 2);
-
-	unsigned int slen = 4;
-	char* charset = "0123456789abcdefghijklmnopqrstuvwxyz";
-	unsigned int clen = strlen(charset);
-	unsigned int l_chains = 1000;
-	unsigned int n_chains = 1000;
-
-	// start / resume table
-	Rainbow_Init(slen, charset, l_chains, n_chains);
-	FILE* f = fopen(argv[1], "r");
-	if (f)
+	if (argc < 6)
 	{
-		Rainbow_FromFile(f);
-		fclose(f);
+		usage(argc, argv);
+		return 1;
 	}
-	signal(SIGINT, signal_handler);
 
-	// generate more chains
-	printf("Generating chains\n");
-	unsigned int c = 0;
-	while (!stop && c < n_chains)
-		c += Rainbow_FindChain();
+	char*        filename = argv[1];
+	char*        charset  = "0123456789abcdefghijklmnopqrstuvwxyz";
+	unsigned int slen     = atoi(argv[2]);
+	unsigned int clen     = strlen(charset);
+	unsigned int l_chains = atoi(argv[3]);
+	unsigned int n_chains = atoi(argv[4]);
+	char*        mode     = argv[5];
 
-	if (!stop)
+	Rainbow_Init(slen, charset, l_chains, n_chains);
+	if (!strcmp(mode, "rtgen"))
 	{
+		// generate more chains
+		printf("Generating chains\n");
+		unsigned int c = 0;
+		while (c < n_chains)
+			c += Rainbow_FindChain();
+
 		printf("Sorting table\n");
 		Rainbow_Sort();
 		printf("Done\n");
+
+		FILE* f = fopen(filename, "w");
+		assert(f);
+		Rainbow_ToFile(f);
+		fclose(f);
 	}
-
-	f = fopen(argv[1], "w");
-	assert(f);
-	Rainbow_ToFile(f);
-	fclose(f);
-	return 0;
-
-	// tests
-	printf("Cracking some hashes\n");
-	char* str = (char*) malloc(slen);
-	memset(str, charset[0], slen);
-	str[slen] = 0;
-	char hash[16];
-	int count = 0;
-	for (unsigned int i = 0; i < clen; i++)
+	else if (!strcmp(mode, "tests"))
 	{
-		str[0] = charset[i];
-		for (unsigned int j = 0; j < clen; j++)
-		{
-			str[1] = charset[j];
-			printf("%i / %i\n", count, i*clen+j);
-			MD5(slen, (uint8_t*) str, (uint8_t*) hash);
-			if (Rainbow_Reverse(hash, NULL))
-				count++;
-		}
-	}
-	printf("%i\n", count);
+		FILE* f = fopen(filename, "r");
+		assert(f);
+		Rainbow_FromFile(f);
+		fclose(f);
 
-	free(str);
+		printf("Cracking some hashes\n");
+		char* str = (char*) malloc(slen);
+		memset(str, charset[0], slen);
+		str[slen] = 0;
+		char hash[16];
+		int count = 0;
+		for (unsigned int i = 0; i < clen; i++)
+		{
+			str[0] = charset[i];
+			for (unsigned int j = 0; j < clen; j++)
+			{
+				str[1] = charset[j];
+				printf("%i / %i\n", count, i*clen+j);
+				MD5(slen, (uint8_t*) str, (uint8_t*) hash);
+				if (Rainbow_Reverse(hash, NULL))
+					count++;
+			}
+		}
+		printf("%i\n", count);
+
+		free(str);
+	}
 	Rainbow_Deinit();
 	return 0;
 }
