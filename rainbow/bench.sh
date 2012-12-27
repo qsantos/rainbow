f5c9a249f5255fe181803b805b46deb1fb0db1f3
Quentin SANTOS <quentin.santos@worldonline.fr>
2012-12-27 17:17:14 +0100

    rainbow: updated benchmark script

--- rainbow/bench.sh
+++ rainbow/bench.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 if [ $# -lt 4 ]
 then
-	printf "Usage: %s slen l_chains n_tests filename\n" $0
+	printf "Usage: %s n_tests filename\n" $0
 	exit 1
 fi
-slen=$1
-l_chains=$2
-n_tests=$3
-filename=$4
+n_tests=$1
+filename=$2
 
 C=0
 T=0
@@ -15,7 +13,7 @@ for i in $(seq $n_tests)
 do
 	string=$(dd if=/dev/urandom 2> /dev/null | tr -cd "[:digit:][:lower:]" | head -c $slen)
 	hash=$(echo -n $string | md5sum | cut -d' ' -f1)
-	if bin/rtcrack $slen $l_chains $hash $filename 2>&1 > /dev/null
+	if bin/rtcrack $hash $filename 2>&1 > /dev/null
 	then
 		C=$(($C+1))
 	fi
