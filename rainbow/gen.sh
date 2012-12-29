2a9543e44db4d24248a31bc12bd186a3b11a7bc7
Quentin SANTOS <quentin.santos@worldonline.fr>
2012-12-29 16:43:48 +0100

    gen.sh auto-organize table files

--- rainbow/gen.sh
+++ rainbow/gen.sh
@@ -9,10 +9,21 @@ l_string=$1
 l_chains=$2
 n_chains=$3
 
+name=alnum_${l_string}_${l_chains}_${n_chains}
+
 shift 3
+
+if [ ! -e rt ]
+then
+	mkdir rt
+fi
+if [ ! -e rt/$name ]
+then
+	mkdir rt/$name
+fi
 
 for i in $(seq $@)
 do
-	bin/rtgen $l_string $i $l_chains $n_chains rt/alnum_${l_string}_${l_chains}_${n_chains}_$i.rt
+	bin/rtgen $l_string $i $l_chains $n_chains rt/${name}/${name}_$i.rt
 	echo $i
 done
