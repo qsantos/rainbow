0a2266db291c72cbc54e9010635e9d7b5ee58de1
Quentin SANTOS <quentin.santos@worldonline.fr>
2012-12-29 18:05:12 +0100

    Taking into account min and max key lengths in rtkey

--- rtkey.c
+++ rtkey.c
@@ -2,17 +2,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <math.h>
 
 typedef unsigned long      u32;
 typedef unsigned long long u64;
 
-void index2key(u64 index, char* key, u32 l_string, const char* charset, u32 n_charset)
+char index2key(u64 index, char* key, u32 l_min, u32 l_max, const char* charset, u32 n_charset)
 {
-	for (u32 i = 0; i < l_string; i++, key++)
+	u32 l_key = l_min;
+	u64 n_key = pow(n_charset, l_min);
+	while (index >= n_key)
+	{
+		index -= n_key;
+		n_key *= n_charset;
+		l_key++;
+	}
+	if (l_key > l_max)
+		return 0;
+
+	for (u32 i = 0; i < l_key; i++, key++)
 	{
 		*key = charset[index % n_charset];
 		index /= n_charset;
 	}
+	return 1;
 }
 
 void usage(int argc, char** argv)
@@ -20,11 +33,12 @@ void usage(int argc, char** argv)
 	(void) argc;
 
 	fprintf(stderr,
-		"Usage: %s l_string n\n"
+		"Usage: %s l_min l_max n\n"
 		"compute the n-th string of the keyspace\n"
 		"\n"
-		"l_string  key length\n"
-		"n         key index\n"
+		"l_min  minimum key length\n"
+		"l_max  maximum key length\n"
+		"n      key index\n"
 		,
 		argv[0]
 	);
@@ -44,22 +58,27 @@ int main(int argc, char** argv)
 		exit(0);
 	}
 
-	if (argc < 3)
+	if (argc < 4)
 	{
 		usage(argc, argv);
 		exit(1);
 	}
 
-	const u32   l_string  = atol(argv[1]);
+	const u32   l_min     = atol(argv[1]);
+	const u32   l_max     = atol(argv[2]);
 	const char* charset   = "0123456789abcdefghijklmnopqrstuvwxyz";
 	const u32   n_charset = strlen(charset);
-	const u64   index     = atoll(argv[2]);
+	const u64   index     = atoll(argv[3]);
 
-	char* string = malloc(l_string);
+	char* string = malloc(l_max+1);
 	assert(string);
-	index2key(index, string, l_string, charset, n_charset);
-	printf("key no %llu = '%s'\n", index, string);
-	free(string);
+	string[l_max] = 0;
 
+	if (index2key(index, string, l_min, l_max, charset, n_charset))
+		printf("key no %llu = '%s'\n", index, string);
+	else
+		printf("key out of range\n");
+
+	free(string);
 	return 0;
 }
