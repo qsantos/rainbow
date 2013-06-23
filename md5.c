53bdb048155f5021d6989255880d0f9c2632cf09
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-06-23 18:53:20 +0200

    Added licence

--- md5.c
+++ md5.c
@@ -1,3 +1,21 @@
+/*\
+ *  This is an awesome programm simulating awesome battles of awesome robot tanks
+ *  Copyright (C) 2012-2013 Quentin SANTOS
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+\*/
+
 // Reference:
 // RFC 1321
 #include "md5.h"
